1aa2f2708b5caec347aa858be8dcfc72564c9d0e
Becca Jonas <beccajonas3@gmail.com>
2024-01-08 22:07:49 -0500

    code along solution

--- server/models.py
+++ server/models.py
@@ -11,6 +11,15 @@ metadata = MetaData(
 
 db = SQLAlchemy(metadata=metadata)
 
+'''
+Employee has many reviews
+A review belongs to one employee
+
+An employee has one onboarding
+
+# ! For a one-to-many relationship, 
+# ! the model on the "many" side is responsible for storing the relationship.
+'''
 
 class Employee(db.Model):
     __tablename__ = "employees"
@@ -19,6 +28,15 @@ class Employee(db.Model):
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
+    # Relationship mapping the employee to related reviews
+    #!  the relationship() construct does not alter the employees or reviews tables. 
+    #! The one-to-many relationship is still stored as a foreign key in the reviews table. 
+    reviews = db.relationship(
+        'Review', back_populates="employee", cascade='all, delete-orphan')
+
+    onboarding = db.relationship(
+        'Onboarding', uselist=False, back_populates='employee', cascade='all, delete-orphan')
+    
     def __repr__(self):
         return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
 
@@ -30,16 +48,28 @@ class Onboarding(db.Model):
     orientation = db.Column(db.DateTime)
     forms_complete = db.Column(db.Boolean, default=False)
 
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping onboarding to related employee
+    employee = db.relationship('Employee', back_populates='onboarding')
+
     def __repr__(self):
         return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
 
 
 class Review(db.Model):
-    __tablename__ = "reviews"
+    __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
 
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping the review to related employee
+    employee = db.relationship('Employee', back_populates="reviews")
+
     def __repr__(self):
-        return f"<Review {self.id}, {self.year}, {self.summary}>"
+        return f'<Review {self.id}, {self.year}, {self.summary}>'
